2baba466ffc5005987006bbcda2c386cff31a36f
tom-gora <goratomasz@outlook.com>
2025-03-03 16:55:12 +0000

    small post tweaks post deployment of say... beta :)

--- lochwinnoch-website/src/middleware.ts
+++ lochwinnoch-website/src/middleware.ts
@@ -2,9 +2,6 @@ export const onRequest = async (context: any, next: Function) => {
   const { request, session } = context;
   const isLoggedIn = await session.get("loggedIn");
   const url = new URL(request.url);
-  const test = Object.fromEntries(request.headers);
-  const referer = test.referer;
-  console.log(test);
 
   if (url.pathname.startsWith("/demo-access")) {
     return isLoggedIn ? context.redirect("/") : next();
